e788f1d2b91849a732e3a562e347efa6f0432f42
Evil-u <yuval.starnes@gmail.com>
2018-07-31 18:33:33 +0300

    Connected to remote mongoDB server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express();
 
 app.get('/',(req,res) => res.send('Hello World') );
+//DB config
+const db = require('./config/keys').mongoURI;
+//connect to mongoDB
+mongoose.
+connect(db,{ useNewUrlParser: true })
 
-const port = process.env.PORT ||5000;
+    .then(()=> console.log('Mongodb connected'))
+    .catch(err => console.log(err));
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
