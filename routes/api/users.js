67c2f756e1bd78951a023a6741ec5af98cdbda35
Evil-u <yuval.starnes@gmail.com>
2018-08-08 15:28:54 +0300

    Login logic inserted and working

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,42 +4,65 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
-
 //sanity check
-router.get ('/test', (req, res) => res.json({msg: "Users Works"})
+router.get('/test', (req, res) => res.json({msg: "Users Works"})
 );
 
 //register
 router.post('/register', (req, res) => {
-User.findOne({email: req.body.email})
-    .then(user => {
-        if (user) {
-            return res.status(400).json({email:'Email Already exists'})
-        } else {
-            const avatar = gravatar.url(req.body.email, {
-                s:'200', // avatar size
-                r:'pg', //avatar rating
-                d: 'mm' // default
-            });
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                avatar,
-                password: req.body.password
-            });
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (user) {
+                return res.status(400).json({email: 'Email Already exists'})
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // avatar size
+                    r: 'pg', //avatar rating
+                    d: 'mm' // default
+                });
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                });
 
-            bcrypt.genSalt(10, (err, salt) => {
-              bcrypt.hash(newUser.password, salt, (err, hash) => {
-               if (err) throw err;
-               newUser.password = hash;
-               newUser
-                   .save()
-                   .then(user => res.json(user))
-                   .catch(err => console.log (err));
-              })
-            })
-        }
-    })
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser
+                            .save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
 });
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find user by email
+    User.findOne({email})
+        .then(user => {
+            // check for user
+            if (!user) {
+                return res.status(404).json({email: 'user not found'});
+            }
+            // vandilate password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({msg: 'success'})
+                    } else {
+                        return res.status(400).json({password: ' password incorrect '});
+                    }
+                })
+        });
+});
+
 
 module.exports = router;
